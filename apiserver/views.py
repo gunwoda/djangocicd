5fcef929510c0a0bdd6ec62318a696acb8113ac9
gunwoda <gunwo3442@gmail.com>
2024-10-21 14:41:17 +0900

    data_update

--- apiserver/views.py
+++ apiserver/views.py
@@ -4,6 +4,6 @@ from rest_framework import status
 
 @api_view(['GET'])
 def index(request):
-    data = {"result":"success","Data":[{"id":"itstudy","name":"Adam"}]}
+    data = {"result1":"success1","Data1":[{"id":"itstudy1","name":"Adam1"}]}
     return Response(data, status=status.HTTP_200_OK)
 
